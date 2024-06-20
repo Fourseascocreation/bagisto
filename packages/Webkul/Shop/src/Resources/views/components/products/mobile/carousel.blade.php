471899250da54944152b71d48211610329abbefe
amit kumar laravel <amitkumar.laravel358@webkul.in>
2024-06-20 17:25:15 +0530

    Fix Web Vitals Issue & some Refactor.

--- packages/Webkul/Shop/src/Resources/views/components/products/mobile/carousel.blade.php
+++ packages/Webkul/Shop/src/Resources/views/components/products/mobile/carousel.blade.php
@@ -51,6 +51,7 @@
                     v-for="(media, index) in [...options.images, ...options.videos]"
                     class="mx-1 h-1.5 w-1.5 cursor-pointer rounded-full"
                     :class="{ 'bg-navyBlue': index === Math.abs(currentIndex), 'opacity-30 bg-gray-500': index !== Math.abs(currentIndex) }"
+                    role="button"
                 >
                 </div>
             </div>
